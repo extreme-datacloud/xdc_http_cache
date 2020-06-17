bf2a10e49f4e757742122d8b5f6c3429987db31f
Federico Fornari <federico.fornari@cnaf.infn.it>
2020-06-17 17:21:08 +0000

    Removed unnecessary CA certs initialization for storm-webdav

--- storage/storm-webdav/assets/scripts/setup.sh
+++ storage/storm-webdav/assets/scripts/setup.sh
@@ -8,10 +8,3 @@ set -ex
 # Setup VOMSDIR
 sudo mkdir -p /etc/grid-security/vomsdir
 sudo cp -r /vomsdir/* /etc/grid-security/vomsdir
-
-# Setup CA CERTS
-if ! [[ `ls /etc/grid-security/certificates/FullchainHostSectigo.pem 2>/dev/null` ]]; then
-    sudo cp /sectigo/FullchainHostSectigo.pem /etc/grid-security/certificates/
-fi
-sudo cp /digicert/FullchainHost.pem /etc/grid-security/certificates/
-sudo cp /digicert/Fullchain.pem /etc/grid-security/certificates/
